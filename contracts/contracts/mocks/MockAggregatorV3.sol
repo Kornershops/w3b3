e75f9d8003204149de03f5d291b0ff83f80907f5
Julius Oghenesuvwe(Suvwe) Omashone <oghenesuvweomashone@gmail.com>
2026-08-25 23:19:40 -0500

    test: add Chainlink feed fixture

---
+++ contracts/contracts/mocks/MockAggregatorV3.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract MockAggregatorV3 {
+    uint8 public immutable decimals;
+    int256 public answer;
+    uint256 public updatedAt;
+    uint80 public answeredInRound;
+
+    constructor(uint8 feedDecimals, int256 initialAnswer) {
+        decimals = feedDecimals;
+        setAnswer(initialAnswer);
+    }
+
+    function setAnswer(int256 newAnswer) public {
+        answer = newAnswer;
+        updatedAt = block.timestamp;
+        answeredInRound += 1;
+    }
+
+    function setUpdatedAt(uint256 timestamp) external {
+        updatedAt = timestamp;
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        return (answeredInRound, answer, updatedAt, updatedAt, answeredInRound);
+    }
+}
